d77fa3990fb8595b249d874b32f5507cfc2d8a76
Dalibor Klobučarić <info@dd-lab.net>
2025-07-23 00:53:41 +0200

    Added version and copyright

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
+// verzija 1.1.0
 /*
- TODO:
- - Dodati podršku za spremanje datoteka
- - Omogućiti učitavanje binarnih .txt fajlova
-*/
-
+ * BinScribe — jednostavna aplikacija za zapisivanje teksta u binarnom obliku
+ * Copyright (C) 2025  Dalibor Klobučarić
+ *
+ * Ovaj program je slobodni softver: možete ga redistribuirati i/ili mijenjati
+ * pod uvjetima GNU General Public License verzije 3 ili novije.
+ *
+ * Ovaj program se distribuira u nadi da će biti koristan,
+ * ali BEZ IKAKVOG JAMSTVA; pogledajte GNU GPL za više detalja.
+ *
+ * Više informacija: https://www.gnu.org/licenses/gpl-3.0.html
+ */
 
 #include "mainwindow.h"
 
--- mainwindow.h
+++ mainwindow.h
@@ -1,4 +1,16 @@
-// verzioja 1.1
+// verzija 1.1.0
+/*
+ * BinScribe — jednostavna aplikacija za zapisivanje teksta u binarnom obliku
+ * Copyright (C) 2025  Dalibor Klobučarić
+ *
+ * Ovaj program je slobodni softver: možete ga redistribuirati i/ili mijenjati
+ * pod uvjetima GNU General Public License verzije 3 ili novije.
+ *
+ * Ovaj program se distribuira u nadi da će biti koristan,
+ * ali BEZ IKAKVOG JAMSTVA; pogledajte GNU GPL za više detalja.
+ *
+ * Više informacija: https://www.gnu.org/licenses/gpl-3.0.html
+ */
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
